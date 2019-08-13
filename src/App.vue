f6d56066bf95054d088f21943fdb4ac963e74405
Fernando Poe Bellentani <contato@fernandob.com.br>
2019-08-13 13:08:15 -0300

    ++

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
       <!-- navbar -->
       <div class="navbar navbar-expand-lg fixed-top navbar-dark">
         <div class="container">
-        <router-link to="/" class="navbar-brand">Acesso.Sorocaba</router-link>
+        <router-link to="/" class="navbar-brand">Acesso.Sorocaba.Br</router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
