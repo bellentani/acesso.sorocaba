0b8644923d3c5f9b1090e4a811bfb8736ecf8fe5
Fernando Poe Bellentani <contato@fernandob.com.br>
2019-08-13 13:55:37 -0300

    Change route

--- src/components/home.vue
+++ src/components/home.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <h1 class="display-4">{{ title }}</h1>
       <p class="lead">Um site prova de conceito sobre acessibilidade para aplicações ricas em Javascript e um local de teste prático de Aria. O objetivo futuro é se tornar uma referência para desenvolvedores que desejem saber um pouco mais sobre acessibilidade.</p>
-      <p>Para isso vocês já podem ir até a lista de links nossas em <a href="/blog">Blog</a> para ver algumas informações relevantes sobre o tema.</p>
+      <p>Para isso vocês já podem ir até a lista de links nossas em <router-link to = "/blog">Blog</router-link> para ver algumas informações relevantes sobre o tema.</p>
     </div>
   </div>
 </template>
